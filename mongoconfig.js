fb4b4513665c73763e27f19f3c04e842d9afbb3f
nero120 <rich.dmz@protonmail.com>
2018-12-30 22:18:07 +0000

    Updated mongodb config script to include TTL index to remove stale syncs.

--- mongoconfig.js
+++ mongoconfig.js
@@ -1,2 +1,3 @@
 db.newsynclogs.createIndex( { "expiresAt": 1 }, { expireAfterSeconds: 0 } );
 db.newsynclogs.createIndex( { "ipAddress": 1 } );
+db.bookmarks.createIndex( { "lastAccessed": 1 }, { expireAfterSeconds: 21*86400 } );
